909df4961de83c9c0f0865a7894b9dedb41683f3
Rlrasmussen <soede_rikke@hotmail.com>
2024-09-26 14:50:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatoriskopgave___Quiz_spil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variabler
+            int liv = 100;
+            string ord; //Find lige på noget bedre Rikke
+            
+            switch (ord)
+            {
+                case 1: "Kage"; break;
+            }
+
+            Console.WriteLine(switch(1));
+            Console.ReadKey();
+        }
+    }
+}
